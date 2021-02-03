fadd9329023831aa33b6f09ac75492b18a6d9d4c
Ernest Gupik <ernestgupik@wp.pl>
2021-02-03 23:28:10 +0100

    Added language: Go.

---
+++ Go/v1/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+    "strconv"
+)
+
+func sumDigits(number int) int {  
+    remainder := 0  
+    sumResult := 0  
+    for number != 0 {  
+        remainder = number % 10  
+        sumResult += remainder  
+        number = number / 10  
+    }  
+    return sumResult
+} 
+
+func gen_key() {
+    s1 := rand.NewSource(time.Now().UnixNano())
+    r1 := rand.New(s1)
+    
+    var first_three_digits string = strconv.Itoa(r1.Intn(999))
+    
+    for first_three_digits=="333" || first_three_digits=="444" || first_three_digits=="555" || first_three_digits=="666" || first_three_digits=="777" || first_three_digits=="888" || first_three_digits=="999" {
+        first_three_digits = strconv.Itoa(r1.Intn(999))
+    }
+    
+    for len(first_three_digits)<3 {
+        first_three_digits += "0"
+    }
+    
+    var num_of_len_seven int = r1.Intn(9999999)
+    var num_added_together int = sumDigits(num_of_len_seven)
+    
+    for num_added_together%7!=0 {
+        num_of_len_seven = r1.Intn(9999999)
+        num_added_together = sumDigits(num_of_len_seven)
+    }
+    
+    var num_of_len_seven_string string = strconv.Itoa(num_of_len_seven)
+    
+    for len(num_of_len_seven_string)<7 {
+        num_of_len_seven_string += "0"
+    }
+    
+    var key string = first_three_digits+"-"+num_of_len_seven_string
+    
+    fmt.Println(key)
+}
+
+func gen_oem() {
+    s1 := rand.NewSource(time.Now().UnixNano())
+    r1 := rand.New(s1)
+    
+    var first_three_digits string = strconv.Itoa(r1.Intn(365)+1)
+    
+    for len(first_three_digits)<3 {
+        first_three_digits += "0"
+    }
+    
+    var second_two_digits string = strconv.Itoa(r1.Intn(8)+95)
+    
+    if len(second_two_digits)==3 {
+        second_two_digits = second_two_digits[1:]
+    }
+    
+    var num_of_len_seven int = r1.Intn(9999999)
+    var num_added_together int = sumDigits(num_of_len_seven)
+    
+    for num_added_together%7!=0 {
+        num_of_len_seven = r1.Intn(9999999)
+        num_added_together = sumDigits(num_of_len_seven)
+    }
+    
+    var num_of_len_seven_string string = strconv.Itoa(num_of_len_seven)
+    
+    for len(num_of_len_seven_string)<7 {
+        num_of_len_seven_string += "0"
+    }
+    
+    var last_five_digits string = strconv.Itoa(r1.Intn(99999))
+    
+    for len(last_five_digits)<5 {
+        last_five_digits += "0"
+    }
+    
+    var key string = first_three_digits+second_two_digits+"-OEM-"+num_of_len_seven_string+"-"+last_five_digits
+    
+    fmt.Println(key)
+}
+
+func main() {
+    fmt.Println("5 CD Keys:")
+    for i := 0; i < 5; i++ {
+        gen_key()
+    }
+    fmt.Println("\n5 CD Keys (OEM):")
+    for i := 0; i < 5; i++ {
+        gen_oem()
+    }
+}
