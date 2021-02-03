5b743ae67a445813bd6c4f7c509d3b7aa4e24ab7
Ernest Gupik <ernestgupik@wp.pl>
2021-02-03 14:01:51 +0100

    Added language: Bash.

---
+++ Bash/v1/main.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+gen_key() {
+    first_three_digits=$[ 1 + $RANDOM % 999 ]
+
+    while [ $first_three_digits -eq 333 ] || [ $first_three_digits -eq 444 ] || [ $first_three_digits -eq 555 ] || [ $first_three_digits -eq 666 ] || 
+            [ $first_three_digits -eq 777 ] || [ $first_three_digits -eq 888 ] || [ $first_three_digits -eq 999 ]
+    do
+        first_three_digits=$[ $RANDOM % 999 ]
+    done
+
+    first_three_digits_len=$(echo $first_three_digits | wc -c)
+
+    while [ $first_three_digits_len -lt 4 ]
+    do
+        first_three_digits=$(echo $first_three_digits\0)
+        first_three_digits_len=$(echo $first_three_digits | wc -c)
+    done
+
+    num_of_len_seven=$[ 1 + $RANDOM * 999 % 9999999 ]
+    num_added_together=0
+
+    for i in $(echo $num_of_len_seven | sed -e 's/\(.\)/\1\n/g')
+    do
+        num_added_together=$(( $num_added_together + $i ))
+    done
+
+    while [ $(( $num_added_together % 7 )) -ne 0 ]
+    do
+        num_of_len_seven=$[ 1 + $RANDOM * 999 % 9999999 ]
+        num_added_together=0
+
+        for i in $(echo $num_of_len_seven | sed -e 's/\(.\)/\1\n/g')
+        do
+            num_added_together=$(( $num_added_together + $i ))
+        done
+    done
+
+    num_of_len_seven_len=$(echo $num_of_len_seven | wc -c)
+
+    while [ $num_of_len_seven_len -lt 8 ]
+    do
+        num_of_len_seven=$(echo $num_of_len_seven\0)
+        num_of_len_seven_len=$(echo $num_of_len_seven | wc -c)
+    done
+
+    key=$(echo $first_three_digits\-$num_of_len_seven)
+
+    echo $key
+}
+
+gen_oem() {
+    first_three_digits=$[ 1 + $RANDOM % 366 ]
+
+    first_three_digits_len=$(echo $first_three_digits | wc -c)
+
+    while [ $first_three_digits_len -lt 4 ]
+    do
+        first_three_digits=$(echo 0$first_three_digits)
+        first_three_digits_len=$(echo $first_three_digits | wc -c)
+    done
+
+    second_two_digits=$[ 95 + $RANDOM % 8 ]
+    second_two_digits_len=$(echo $second_two_digits | wc -c)
+
+    if [ $second_two_digits_len -eq 4 ]
+    then
+        second_two_digits=${second_two_digits:1}
+    fi
+
+    num_of_len_seven=$[ 1 + $RANDOM * 999 % 9999999 ]
+    num_added_together=0
+
+    for i in $(echo $num_of_len_seven | sed -e 's/\(.\)/\1\n/g')
+    do
+        num_added_together=$(( $num_added_together + $i ))
+    done
+
+    while [ $(( $num_added_together % 7 )) -ne 0 ]
+    do
+        num_of_len_seven=$[ 1 + $RANDOM * 999 % 9999999 ]
+        num_added_together=0
+
+        for i in $(echo $num_of_len_seven | sed -e 's/\(.\)/\1\n/g')
+        do
+            num_added_together=$(( $num_added_together + $i ))
+        done
+    done
+
+    num_of_len_seven_len=$(echo $num_of_len_seven | wc -c)
+
+    while [ $num_of_len_seven_len -lt 8 ]
+    do
+        num_of_len_seven=$(echo $num_of_len_seven\0)
+        num_of_len_seven_len=$(echo $num_of_len_seven | wc -c)
+    done
+
+    last_five_digits=$[ 1 + $RANDOM * 99 % 99999 ]
+    last_five_digits_len=$(echo $last_five_digits | wc -c)
+
+    while [ $last_five_digits_len -lt 6 ]
+    do
+        last_five_digits=$(echo $last_five_digits\0)
+        last_five_digits_len=$(echo $last_five_digits | wc -c)
+    done
+
+    key=$(echo $first_three_digits$second_two_digits\-OEM-$num_of_len_seven\-$last_five_digits)
+
+    echo $key
+}
+
+main() {
+    echo "5 CD Keys:"
+    for i in {1..500}
+    do
+        gen_key
+    done
+    echo ""
+    echo "5 CD Keys (OEM):"
+    for i in {1..500}
+    do
+        gen_oem
+    done
+}
+
+main
