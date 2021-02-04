00d70c7536640faf28f3999fbae4436faa3427ed
Ernest Gupik <ernestgupik@wp.pl>
2021-02-04 11:21:14 +0100

    Fixed Java implementation.

--- Java/v1/main.java
+++ Java/v1/main.java
@@ -51,7 +51,7 @@ public class Main
             second_two_digits = second_two_digits.substring(1);
         }
         
-        String num_of_len_seven = Integer.toString((int)Math.floor(Math.random() * (9999999 + 1)));
+        String num_of_len_seven = Integer.toString((int)Math.floor(Math.random() * (999999 + 1)));
         int num_added_together = 0;
         
         for(int i=0; i<num_of_len_seven.length(); i++) {
@@ -59,7 +59,7 @@ public class Main
         }
         
         while(num_added_together%7!=0) {
-            num_of_len_seven = Integer.toString((int)Math.floor(Math.random() * (9999999 + 1)));
+            num_of_len_seven = Integer.toString((int)Math.floor(Math.random() * (999999 + 1)));
             num_added_together = 0;
             
             for(int i=0; i<num_of_len_seven.length(); i++) {
@@ -67,7 +67,7 @@ public class Main
             }
         }
         
-        while(num_of_len_seven.length()<7) {
+        while(num_of_len_seven.length()<6) {
             num_of_len_seven += "0";
         }
         
@@ -77,7 +77,7 @@ public class Main
             last_five_digits += "0";
         }
         
-        String key = first_three_digits+second_two_digits+"-OEM-"+num_of_len_seven+"-"+last_five_digits;
+        String key = first_three_digits+second_two_digits+"-OEM-0"+num_of_len_seven+"-"+last_five_digits;
         
         System.out.println(key);
     }
