3464f76f03800a43ea6353fa29ea57f110f466b9
Ernest Gupik <ernestgupik@wp.pl>
2021-02-05 14:58:03 +0100

    PHP: rename num_of_len_seven to mul_of_seven.

--- PHP/v1/main.php
+++ PHP/v1/main.php
@@ -11,29 +11,29 @@ function gen_key() {
         $first_three_digits .= 0;
     }
     
-    $num_of_len_seven = rand(0, 9999999);
+    $mul_of_seven = rand(0, 9999999);
     $num_added_together = 0;
-    $num_array = str_split((string)$num_of_len_seven);
+    $num_array = str_split((string)$mul_of_seven);
     
     foreach($num_array as $number) {
         $num_added_together += $number;
     }
     
     while($num_added_together%7!=0) {
-        $num_of_len_seven = rand(0, 9999999);
+        $mul_of_seven = rand(0, 9999999);
         $num_added_together = 0;
-        $num_array = str_split((string)$num_of_len_seven);
+        $num_array = str_split((string)$mul_of_seven);
         
         foreach($num_array as $number) {
             $num_added_together += $number;
         }
     }
     
-    while(strlen((string)$num_of_len_seven)<7) {
-        $num_of_len_seven .= 0;
+    while(strlen((string)$mul_of_seven)<7) {
+        $mul_of_seven .= 0;
     }
     
-    $key = (string)$first_three_digits."-".(string)$num_of_len_seven;
+    $key = (string)$first_three_digits."-".(string)$mul_of_seven;
     
     echo $key."\n";
 }
@@ -51,26 +51,26 @@ function gen_oem() {
         $second_two_digits = substr($second_two_digits, 1);
     }
     
-    $num_of_len_seven = rand(0, 999999);
+    $mul_of_seven = rand(0, 999999);
     $num_added_together = 0;
-    $num_array = str_split((string)$num_of_len_seven);
+    $num_array = str_split((string)$mul_of_seven);
     
     foreach($num_array as $number) {
         $num_added_together += $number;
     }
     
     while($num_added_together%7!=0) {
-        $num_of_len_seven = rand(0, 999999);
+        $mul_of_seven = rand(0, 999999);
         $num_added_together = 0;
-        $num_array = str_split((string)$num_of_len_seven);
+        $num_array = str_split((string)$mul_of_seven);
         
         foreach($num_array as $number) {
             $num_added_together += $number;
         }
     }
     
-    while(strlen((string)$num_of_len_seven)<6) {
-        $num_of_len_seven .= 0;
+    while(strlen((string)$mul_of_seven)<6) {
+        $mul_of_seven .= 0;
     }
     
     $last_five_digits = rand(0, 99999);
@@ -78,7 +78,7 @@ function gen_oem() {
         $last_five_digits .= 0;
     }
     
-    $key = $first_three_digits.$second_two_digits."-OEM-0".$num_of_len_seven."-".$last_five_digits;
+    $key = $first_three_digits.$second_two_digits."-OEM-0".$mul_of_seven."-".$last_five_digits;
     
     echo $key."\n";
 }
