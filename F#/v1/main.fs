6dd4ff029dccd2974174b38787d69282591d7481
Ernest Gupik <ernestgupik@wp.pl>
2021-02-05 15:07:39 +0100

    F#: rename num_of_len_seven to mul_of_seven.

--- F#/v1/main.fs
+++ F#/v1/main.fs
@@ -9,29 +9,29 @@ let gen_key arg =
     while (String.length first_three_digits<3) do
         first_three_digits <- first_three_digits+"0"
     
-    let mutable num_of_len_seven = (new System.Random()).Next(0, 9999999)
-    let mutable num_of_len_seven_copy = num_of_len_seven
-    let mutable num_of_len_seven_final = int2String (num_of_len_seven)
+    let mutable mul_of_seven = (new System.Random()).Next(0, 9999999)
+    let mutable mul_of_seven_copy = mul_of_seven
+    let mutable mul_of_seven_final = int2String (mul_of_seven)
     let mutable num_added_together = 0
     
-    for i=0 to String.length num_of_len_seven_final do
-        num_added_together <- num_added_together +  (num_of_len_seven_copy%10)
-        num_of_len_seven_copy <- num_of_len_seven_copy / 10
+    for i=0 to String.length mul_of_seven_final do
+        num_added_together <- num_added_together +  (mul_of_seven_copy%10)
+        mul_of_seven_copy <- mul_of_seven_copy / 10
         
     while (not(num_added_together%7=0)) do
-        num_of_len_seven <- (new System.Random()).Next(0, 9999999)
-        num_of_len_seven_copy <- num_of_len_seven
-        num_of_len_seven_final <- int2String (num_of_len_seven)
+        mul_of_seven <- (new System.Random()).Next(0, 9999999)
+        mul_of_seven_copy <- mul_of_seven
+        mul_of_seven_final <- int2String (mul_of_seven)
         num_added_together <- 0
         
-        for i=0 to String.length num_of_len_seven_final do
-            num_added_together <- num_added_together +  (num_of_len_seven_copy%10)
-            num_of_len_seven_copy <- num_of_len_seven_copy / 10
+        for i=0 to String.length mul_of_seven_final do
+            num_added_together <- num_added_together +  (mul_of_seven_copy%10)
+            mul_of_seven_copy <- mul_of_seven_copy / 10
             
-    while (String.length num_of_len_seven_final<7) do
-        num_of_len_seven_final <- num_of_len_seven_final+"0"
+    while (String.length mul_of_seven_final<7) do
+        mul_of_seven_final <- mul_of_seven_final+"0"
 
-    let key = first_three_digits+"-"+num_of_len_seven_final
+    let key = first_three_digits+"-"+mul_of_seven_final
 
     printfn "%s" key
     
@@ -46,34 +46,34 @@ let gen_oem arg =
     if (String.length second_two_digits=3) then
         second_two_digits <- second_two_digits.[1..]
 
-    let mutable num_of_len_seven = (new System.Random()).Next(0, 999999)
-    let mutable num_of_len_seven_copy = num_of_len_seven
-    let mutable num_of_len_seven_final = int2String (num_of_len_seven)
+    let mutable mul_of_seven = (new System.Random()).Next(0, 999999)
+    let mutable mul_of_seven_copy = mul_of_seven
+    let mutable mul_of_seven_final = int2String (mul_of_seven)
     let mutable num_added_together = 0
     
-    for i=0 to String.length num_of_len_seven_final do
-        num_added_together <- num_added_together +  (num_of_len_seven_copy%10)
-        num_of_len_seven_copy <- num_of_len_seven_copy / 10
+    for i=0 to String.length mul_of_seven_final do
+        num_added_together <- num_added_together +  (mul_of_seven_copy%10)
+        mul_of_seven_copy <- mul_of_seven_copy / 10
         
     while (not(num_added_together%7=0)) do
-        num_of_len_seven <- (new System.Random()).Next(0, 999999)
-        num_of_len_seven_copy <- num_of_len_seven
-        num_of_len_seven_final <- int2String (num_of_len_seven)
+        mul_of_seven <- (new System.Random()).Next(0, 999999)
+        mul_of_seven_copy <- mul_of_seven
+        mul_of_seven_final <- int2String (mul_of_seven)
         num_added_together <- 0
         
-        for i=0 to String.length num_of_len_seven_final do
-            num_added_together <- num_added_together +  (num_of_len_seven_copy%10)
-            num_of_len_seven_copy <- num_of_len_seven_copy / 10
+        for i=0 to String.length mul_of_seven_final do
+            num_added_together <- num_added_together +  (mul_of_seven_copy%10)
+            mul_of_seven_copy <- mul_of_seven_copy / 10
             
-    while (String.length num_of_len_seven_final<6) do
-        num_of_len_seven_final <- num_of_len_seven_final+"0"
+    while (String.length mul_of_seven_final<6) do
+        mul_of_seven_final <- mul_of_seven_final+"0"
 
     let mutable last_five_digits = int2String ((new System.Random()).Next(0, 99999))
     
     while (String.length last_five_digits<5) do
         last_five_digits <- last_five_digits+"0"
 
-    let key = first_three_digits+second_two_digits+"-OEM-O"+num_of_len_seven_final+"-"+last_five_digits
+    let key = first_three_digits+second_two_digits+"-OEM-O"+mul_of_seven_final+"-"+last_five_digits
 
     printfn "%s" key
 
