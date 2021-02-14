d3b35f385f24c7a7b2f9c0bf554602b0538b26d4
Ernest Gupik <ernestgupik@wp.pl>
2021-02-14 12:46:11 +0100

    Added better version of R implementation (v2).

---
+++ R/v2/main.r
@@ -0,0 +1,135 @@
+gen_key <- function() {
+    # generating random number in range from 0 to 999 (max 3 digit number)
+    first_three_digits = sprintf("%d", floor(runif(1, 0, 999+1)))
+
+    while(first_three_digits=="333" || first_three_digits=="444" || first_three_digits=="555" || first_three_digits=="666" 
+        || first_three_digits=="777" || first_three_digits=="888" || first_three_digits=="999") {
+        # regenerating random number to the point when they are not repeating (excluding 111 and 222)
+        first_three_digits = sprintf("%d", floor(runif(1, 0, 999+1)))
+    }
+
+    while(nchar(first_three_digits)<3) {
+        # adding trailing zeros if necessary (to match length of 3)
+        first_three_digits = paste(first_three_digits, "0", sep="")
+    }
+
+    # generating random number in range from 0 to 999999 (max 6 digit number)
+    mul_of_seven = sprintf("%d", floor(runif(1, 0, 999999+1)))
+
+    # creating variable which will be use to count sum of all digits generated in the previous step
+    num_added_together = 0
+
+    # creating list of digits from number contained in mul_of_seven variable
+    mul_of_seven_split = strsplit(mul_of_seven, "")[[1]]
+
+    # iterating over each digit in list mul_of_seven_split
+    for(i in mul_of_seven_split) {
+        # adding digit to variable num_added_together
+        num_added_together = num_added_together + strtoi(i, 10)
+    }
+
+    while(nchar(mul_of_seven)<6) {
+        # adding trailing zeros if necessary (to match length of 6)
+        mul_of_seven = paste(mul_of_seven, "0", sep="")
+    }
+
+    # searching for remainder value to make mul_of_seven divisible by 7
+    for(i in 0:6) {
+        if((num_added_together+(i+1))%%7==0) {
+            # adding this value to the end of mul_of_seven variable
+            mul_of_seven = paste(mul_of_seven, sprintf("%d", i+1), sep="")
+            break
+        }
+    }
+
+    # combining all needed variables with dashes and other constants into variable named key
+    key = paste(first_three_digits, mul_of_seven, sep="-")
+
+    # displaying generated key to user
+    print(key)
+}
+
+gen_oem <- function() {
+    # generating random number in range from 1 to 366
+    first_three_digits = sprintf("%d", floor(runif(1, 1, 366+1)))
+
+    while(nchar(first_three_digits)<3) {
+        # adding zeros at the beginning if necessary (to match length of 3)
+        temp = first_three_digits
+        first_three_digits = paste("0", temp, sep="")
+    }
+
+    # generating random number in range from 95 to 102
+    second_two_digits = sprintf("%d", floor(runif(1, 95, 102+1)))
+
+    if(nchar(second_two_digits)==3) {
+        # if generated number is 3 digits long then remove first digit
+        second_two_digits = substr(second_two_digits, 2, 3)
+    }
+
+    # generating random number in range from 0 to 99999 (max 5 digit number)
+    mul_of_seven = sprintf("%d", floor(runif(1, 0, 99999+1)))
+
+    # creating variable which will be use to count sum of all digits generated in the previous step
+    num_added_together = 0
+
+    # creating list of digits from number contained in mul_of_seven variable
+    mul_of_seven_split = strsplit(mul_of_seven, "")[[1]]
+
+    # iterating over each digit in list mul_of_seven_split
+    for(i in mul_of_seven_split) {
+        # adding digit to variable num_added_together
+        num_added_together = num_added_together + strtoi(i, 10)
+    }
+
+    while(nchar(mul_of_seven)<5) {
+        # adding trailing zeros if necessary (to match length of 5)
+        mul_of_seven = paste(mul_of_seven, "0", sep="")
+    }
+
+    # searching for remainder value to make mul_of_seven divisible by 7
+    for(i in 0:6) {
+        if((num_added_together+(i+1))%%7==0) {
+            # adding this value to the end of mul_of_seven variable
+            mul_of_seven = paste(mul_of_seven, sprintf("%d", i+1), sep="")
+            break
+        }
+    }
+
+    # generating random number in range from 0 to 99999 (max 5 digit number)
+    last_five_digits = sprintf("%d", floor(runif(1, 0, 99999+1)))
+
+    while(nchar(last_five_digits)<5) {
+        # adding trailing zeros if necessary (to match length of 5)
+        last_five_digits = paste(last_five_digits, "0", sep="")
+    }
+
+    # combining all needed variables with dashes and other constants into variable named key
+    key = paste(first_three_digits, second_two_digits, sep="")
+    key = paste(key, mul_of_seven, sep="-OEM-0")
+    key = paste(key, last_five_digits, sep="-")
+
+    # displaying generated key to user
+    print(key)
+}
+
+main <- function() {
+
+    print("5 CD Keys:")
+
+    # generating 5 keys (and displaying them)
+    for(i in 1:5) {
+        gen_key()
+    }
+
+    print("")
+    print("5 CD Keys (OEM):")
+
+    # generating 5 OEM keys (and displaying them)
+    for(i in 1:5) {
+        gen_oem()
+    }
+}
+
+# executing main function
+main()
